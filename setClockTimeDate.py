cd08f7a95a59eb90b91dd766c6406231d5005a9b
invchar <16106734+invchar@users.noreply.github.com>
2019-02-06 16:55:13 -0600

    Update setClockTimeDate.py
    Update logged text strings to remove redundant prefix, plus fix one string missing curly braces.

--- setClockTimeDate.py
+++ setClockTimeDate.py
@@ -20,20 +20,20 @@ def updateClock(clock):
 		browser["FDateString"] = time.strftime("%y%m%d")
 		browser["FTimeString"] = time.strftime("%H%M%S")
 		browser.submit_selected()
-		log(message="setClockTimeDate.py: Clock {} has been updated".format(clock))
+		log(message="Clock {} has been updated".format(clock))
 		return 0
 	except Exception as e:
-		log(LOG_ERR, "setClockTimeDate.py: Failed to update clock at ".format(clock))
+		log(LOG_ERR, "Failed to update clock at {}".format(clock))
 		log(LOG_ERR, str(e))
 		return 1
 
 def main():
 	if (not user or not password or not clocks):
-		log(LOG_ERR,"setClockTimeDate.py: Not all environment variables are set")
+		log(LOG_ERR,"Not all environment variables are set")
 		sys.exit(1)
 	for clock in clocks:
 		if (updateClock(clock)):
-			log(LOG_ERR, "seClockTimeDate.py: {} was not updated".format(clock))
+			log(LOG_ERR, "{} was not updated".format(clock))
 
 if __name__ == "__main__":
 	main()
